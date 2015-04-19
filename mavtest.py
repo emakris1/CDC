dd905a80f97ca6f2680e27c586eb5ef0226095a6
Erick Makris <nox357@gmail.com>
2015-04-19 16:36:53 -0400

    Fixed mission algorithm bugs after CDC integration

--- mavtest.py
+++ mavtest.py
@@ -5,13 +5,15 @@ from pymavlink import mavutil
 # open mavlink connection
 mvlnk = mavutil.mavlink_connection(device='/dev/ttyUSB0', baud=57600)
 
-# fetch the initial sequence number
 while True:
     msg = mvlnk.recv_match(type='GLOBAL_POSITION_INT', blocking=True, timeout=1)
-    print 'GPS Lat: ' + str(msg.lat)
-    print 'GPS Long: ' + str(msg.lon)
-    print 'Heading: ' + str(msg.hdg / 100.0)
-    print 'Altitude: ' + str(msg.relative_alt / 1000.0)
-    print 'Velocity (X): ' + str(msg.vx)
-    print 'Velocity (Y): ' + str(msg.vy)
-    print 'Velocity (Z): ' + str(msg.vz)
+    if msg:
+        print 'GPS Lat: ' + str(msg.lat)
+        print 'GPS Long: ' + str(msg.lon)
+        print 'Heading: ' + str(msg.hdg / 100.0)
+        print 'Altitude: ' + str(msg.relative_alt / 1000.0)
+        print 'Velocity (X): ' + str(msg.vx)
+        print 'Velocity (Y): ' + str(msg.vy)
+        print 'Velocity (Z): ' + str(msg.vz)
+    else:
+        print 'Nothing'
